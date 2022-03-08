0dace90261916fc9f6cb87f3f32d6b901e14b575
woose28 <dldntmdsla@naver.com>
2022-03-08 20:56:49 +0900

    chore: webpack loader 및 plugin 설정

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
 const path = require('path');
+const webpack = require('webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: process.env.NODE_ENV,
   entry: {
     main: './src/index.js',
   },
@@ -9,4 +13,27 @@ module.exports = {
     filename: '[name].js',
     path: path.resolve(__dirname, './dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          process.env.NODE_ENV === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: () => `Build Time: ${new Date().toLocaleString()}`,
+    }),
+    new CleanWebpackPlugin(),
+    ...(process.env.NODE_ENV === 'production'
+      ? [new MiniCssExtractPlugin({ filename: '[name].css' })]
+      : []),
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
 };
